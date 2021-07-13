05621eb8b49eefbb216ef76687f4d94c25ada842
Efrat Granit <efrat.gnt@gmail.com>
2021-07-13 03:18:47 +0300

    complete-project

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 error_char = 'expected only C A T G chars string'
 ba_commands = ['new', 'load', 'dup', 'del', 'save', 'slice', 'replace', 'len', 'find', 'findall']
 commands = ba_commands + ['batch', 'run', 'batchlist']
-allow = ['C', 'A', 'G', 'T']
+allow = ['G', 'C', 'A', 'T']
 ending = '.rawdna'
